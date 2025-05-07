61fa6b19ea02d5b13321f588fc45c85a3ecd960c
Jeganathan Swaminathan <mail2jegan@gmail.com>
2025-05-07 17:41:08 +0530

    Added Day3 lab exercises

---
+++ Day3/terraform/local-exec/inputs.tf
@@ -0,0 +1,11 @@
+variable "container_name1" {
+  description = "Name of the container"
+  type = string
+  default = "ubuntu_container1"
+}
+
+variable "container_name2" {
+  description = "Name of the container"
+  type = string
+  default = "rocky_container1"
+}
